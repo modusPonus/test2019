4341e007071cfa43cef18e3647ad5077d4c5236f
modusPonus <48015262+modusPonus@users.noreply.github.com>
2019-11-11 23:44:38 +0200

    CaesarTest

---
+++ TestTask/TestTask/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace TestTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Silver;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Black;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(357, 405);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Silver;
+            this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox2.Location = new System.Drawing.Point(485, 33);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(357, 405);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Gray;
+            this.button1.Enabled = false;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(385, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "------>";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(379, 418);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Visible = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Gray;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(854, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(854, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(870, 489);
+            this.MinimumSize = new System.Drawing.Size(870, 489);
+            this.Name = "Form1";
+            this.Text = "TestCaesar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ TestTask/TestTask/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace TestTask
+{
+    public partial class Form1 : Form
+    {
+        private char[] massOfChar = {'а','б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', };
+        private Hashtable hashtableText = new Hashtable();
+        private Hashtable hashtableFinValue = new Hashtable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hashtableText = GetHashtableWithNumOfText(richTextBox1.Text);
+            hashtableFinValue = FreqCalculation(hashtableText, GetNumOfText(richTextBox1.Text));
+            int shift = FindShift(hashtableFinValue);
+            richTextBox2.Text = BuildText(shift);
+
+            hashtableText.Clear();
+            hashtableFinValue.Clear();
+        }
+
+        private Hashtable GetHashtableWithNumOfText(String text) 
+        {
+            Hashtable hashtable = new Hashtable();
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (hashtable.ContainsKey(text[i]))
+                    {
+                        hashtable[text[i]] = (int)hashtable[text[i]] + 1;
+                    }
+                    else
+                    {
+                        hashtable.Add(text[i], 1);
+                    }
+                }
+            }
+            return hashtable;
+        }
+
+        private int GetNumOfText(String text)
+        {
+            String textFin = "";
+            text = text.ToLower();
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    textFin = textFin + text[i];
+                }
+            }
+            return textFin.Length;
+        }
+
+        private Hashtable FreqCalculation(Hashtable hashtable, int textNum)
+        {
+            Hashtable hashtableValue = new Hashtable();
+            ICollection keys = hashtable.Keys;
+            foreach (char Text in keys)
+            {
+                double num = (int)hashtable[Text];
+                double freq = Math.Round((num / textNum * 100), 2);
+                if (hashtableValue.ContainsKey(Text))
+                {
+                    hashtableValue[Text] = Math.Round(freq, 2);
+                }
+                else
+                {
+                    hashtableValue.Add(Text, Math.Round(freq, 2));
+                }
+            }
+            return hashtableValue;
+        }
+
+        private int FindShift(Hashtable hashtable)
+        {
+            ICollection keys = hashtable.Keys;
+            double dif = Double.MinValue;
+            String finch = "";
+            foreach (char text in keys)
+            {
+                textBox1.Text = textBox1.Text + (hashtable[text]);
+                double count = Convert.ToDouble(textBox1.Text);
+                textBox1.Text = "";
+                if (count > dif)
+                {
+                    dif = count;
+                    textBox1.Text = textBox1.Text + text;
+                    finch = textBox1.Text;
+                    textBox1.Text = "";
+                }
+            }
+            char[] shiftChar = finch.ToCharArray();
+            int shift = Array.IndexOf(massOfChar, 'о') - Array.IndexOf(massOfChar, shiftChar[0]);
+
+            return shift;
+        }
+
+        private String BuildText(int shift)
+        {
+            String textFin = "";
+            String text = richTextBox1.Text;
+            int ind;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLetter(text[i]))
+                {
+                    if (char.IsUpper(text[i]) == true)
+                    {
+                        if (shift > 0) { ind = Array.IndexOf(massOfChar, char.ToLower(text[i])) - shift; }
+                        else { ind = Array.IndexOf(massOfChar, char.ToLower(text[i])) + shift; }
+                        if (ind < 0)
+                        {
+                            textFin = textFin + massOfChar[massOfChar.Length + ind];
+                        }
+                        else { textFin = textFin + char.ToUpper(massOfChar[ind]); }
+                    }
+                    else
+                    {
+                        if (shift > 0) { ind = Array.IndexOf(massOfChar, text[i]) - shift; }
+                        else { ind = Array.IndexOf(massOfChar, text[i]) + shift; }
+                        if (ind < 0)
+                        {
+                            textFin = textFin + massOfChar[massOfChar.Length + ind];
+                        }
+                        else { textFin = textFin + massOfChar[ind]; }
+                    }
+                }
+                else { textFin = textFin + text[i]; }
+            }
+            return textFin;
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length < 1000)
+            {
+                button1.Enabled = false;
+            } else { button1.Enabled = true; }
+        }
+    }
+}
